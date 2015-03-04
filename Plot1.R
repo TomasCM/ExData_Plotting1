d13fb2d6cc676b44a5e964aabf676f05407915a3
TomasCM <olivo_5@hotmail.com>
2015-03-04 18:14:55 +0100

    Revisión plot 1

--- Plot1.R
+++ Plot1.R
@@ -1,5 +1,8 @@
 # Plot1 ---- project 1
+tag <- c("^[12]/2/2007|^Date")
 data<-grep(tag, readLines("household_power_consumption.txt"), value=TRUE)
+writeLines(data, "hpc_new.txt")
+data<- read.table("hpc_new.txt",sep=";",header=T,na.strings="?")
 data$Date <- strptime(paste(data$Date, data$Time, sep =" " ), "%d/%m/%Y %H:%M:%S")
 png(filename="Plot1.png", width=480, height=480)
 hist(data$Global_active_power, col="red", main="Global Active Power",xlab=("Global Active Power(kilowatts)"))
